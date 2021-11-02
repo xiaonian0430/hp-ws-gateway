edac512a90aa88a7f101d5030107001764d878b6
xiaonian0430 <xiaonian0430@163.com>
2021-11-02 16:53:58 +0800

    添加协程

--- config/dev.php
+++ config/dev.php
@@ -4,7 +4,10 @@
  * @contact: xiaonian030@163.com
  * @datetime: 2019-12-01 14:01
  */
+
+//SWOOLE 1 其他 0
 return [
+    'EVENT_LOOP'=>1,
     'REGISTER'    => [
         'LAN_IP'         => '172.16.1.174',
         'LAN_PORT'       => 1236
